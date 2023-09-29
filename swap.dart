501db24c23b98cf397f91701ce4fe7c4bd50d795
SAFVAN EKKADAN <msafvanek@gmail.com>
2023-09-29 14:09:42 +0530

    new

---
+++ swap.dart
@@ -0,0 +1,15 @@
+void main(){
+  double num1=5.6;
+  double num2= 5.8;
+
+  print("before swapping:");
+
+  print("num1 =$num1");
+  print("num2=$num2");
+  double temp =num1;
+  num1 =num2;
+  num2= temp;
+  print("\nafter swapping:");
+  print("num1 =$num1");
+  print("num2=$num2");
+}
