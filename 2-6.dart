a5988e198613e4382f4375705290a12ef06c296d
SAFVAN EKKADAN <msafvanek@gmail.com>
2023-10-07 12:13:55 +0530

    push

---
+++ 2-6.dart
@@ -0,0 +1,11 @@
+//mutliplier tablet 5 
+void main() {
+  int multiplier = 5;
+
+  print("Multiplication Table of $multiplier:");
+
+  for (int i = 1; i <= 10; i++) {
+    int result = multiplier * i;
+    print("$multiplier x $i = $result");
+  }
+}
